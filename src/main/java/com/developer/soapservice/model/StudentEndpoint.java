ab5e637f497f74a5d2634425357fbd7a3bd52dd5
Ajay Koranne <akoranne@gmail.com>
2018-05-11 06:34:29 -0500

    init checkin

---
+++ src/main/java/com/developer/soapservice/model/StudentEndpoint.java
@@ -0,0 +1,33 @@
+package com.developer.soapservice.model;
+
+import com.developer.soapservice.repository.StudentRepository;
+import com.developer.xml.generated.StudentDetailsRequest;
+import com.developer.xml.generated.StudentDetailsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class StudentEndpoint {
+	private static final String NAMESPACE_URI = "http://www.developer.com/xml/generated";
+
+	@Autowired
+	private StudentRepository StudentRepository;
+
+	@Autowired
+	public StudentEndpoint(StudentRepository StudentRepository) {
+		this.StudentRepository = StudentRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "StudentDetailsRequest")
+	@ResponsePayload
+	public StudentDetailsResponse getStudent(@RequestPayload StudentDetailsRequest request) {
+		StudentDetailsResponse response = new StudentDetailsResponse();
+		response.setStudent(StudentRepository.findStudent(request.getName()));
+
+		return response;
+	}
+
+}
